ffd04a31d42bdc8d7d7578f94def4fa2e86cdd2b
Brandon Mayer <b.mayer1@gmail.com>
2014-12-19 11:36:34 -0500

    first working copy with spbasic bibliography style.

--- book/buildPdf.sh
+++ book/buildPdf.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+if [ -e ci14book_mayer.aux ]; then
+   rm ci14book_mayer.aux
+fi
+
+if [ -e ci14book_mayer.pdf ]; then
+    rm ci14book_mayer.pdf
+fi
+
 if pdflatex ci14book_mayer.tex && \
 bibtex ci14book_mayer.aux && \
 pdflatex ci14book_mayer.tex && \
